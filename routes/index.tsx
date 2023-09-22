aa8815ee3ddaf9ab9147b87559bdfce8ea3e3242
hasundue <hasundue@gmail.com>
2023-09-22 14:44:46 +0900

    Format the code

--- routes/index.tsx
+++ routes/index.tsx
@@ -14,12 +14,19 @@ export default function Home() {
         <h2>chiezo.dev</h2>
         <p>Powered by:</p>
         <ul>
-          <li><a href="https://deno.land/deploy">Deno Deploy</a></li>
-          <li><a href="https://fresh.deno.dev">Fresh</a></li>
-          <li><a href="https://simplecss.org">Simple.css</a></li>
+          <li>
+            <a href="https://deno.land/deploy">Deno Deploy</a>
+          </li>
+          <li>
+            <a href="https://fresh.deno.dev">Fresh</a>
+          </li>
+          <li>
+            <a href="https://simplecss.org">Simple.css</a>
+          </li>
         </ul>
         <p>
-          <a href="https://github.com/hasundue/chiezo.dev">Source (GitHub)</a><br />
+          <a href="https://github.com/hasundue/chiezo.dev">Source (GitHub)</a>
+          <br />
         </p>
       </div>
     </>
